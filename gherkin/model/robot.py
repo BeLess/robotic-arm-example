fd90d4ab1d11f545c5e3948dc6ad1223e180edd9
BeLess <benaless12@gmail.com>
2021-11-26 22:49:30 -0500

    Further fixes and improvements to the rotation problem

--- gherkin/model/robot.py
+++ gherkin/model/robot.py
@@ -14,9 +14,9 @@ class Robot:
     base: RotatingBase = field(default_factory=RotatingBase)
 
     def reach(self, goal: Goal, vis) -> None:
-        running = True
         found_angle = False
-        success= False
+        success = False
+        goal = self._evaluate_goal(goal)
         goal_theta_0, goal_theta_1 = self.arm.inverse(goal.x, goal.y)
 
         while not success:
@@ -47,7 +47,7 @@ class Robot:
         Check that the robots angle matches the angle of the goal.
         Because the arm can reach "behind" the point of rotation, reflective angles are functionally equivalent
         """
-        return (self.base.angle == goal.angle) or ((self.base.angle - 180) == goal.angle)
+        return (self.base.angle == goal.angle) or (self.base.angle.inverse == goal.angle)
 
     def _rotate(self, rotation: Rotation):
         self.base.rotate(rotation.direction, rotation.speed)
@@ -64,10 +64,26 @@ class Robot:
         return Robot
 
     def _determine_rotation(self, goal: Goal) -> Rotation:
-        difference_cw = abs(self.base.angle.angle - goal.angle.angle)
-        difference_ccw = abs(self.base.angle.inverse.angle - goal.angle.angle)
-        direction, difference = (DIRECTION.CLOCKWISE, difference_cw) if difference_cw < difference_ccw \
-            else (DIRECTION.COUNTER_CLOCKWISE, difference_ccw)
+        direction = DIRECTION.CLOCKWISE if ((goal.angle - self.base.angle).angle % 360 < 180) else DIRECTION.COUNTER_CLOCKWISE
+
+        difference = abs(self.base.angle.angle - goal.angle.angle)
         speed = SPEED.FAST if difference > self.base.limits.FAST_ROTATION_SPEED else SPEED.FINE
 
         return Rotation(direction, speed)
+
+    def _evaluate_goal(self, goal: Goal) -> Goal:
+        """
+        This is to handle the concept of "facing" when it comes to the angle of rotation
+        Without this transformation, negative x values (spatially understood as "behind") at angles
+        that would cause the robot to rotate counterclockwise (any angle over 90 degrees) would
+        result in a nonsensical result (rotating "behind" and reaching "forward" for a -x value)
+        """
+
+        optimal_angle = goal.angle if self.base.angle + 90 > goal.angle else goal.angle.inverse
+        goal = Goal(
+            x=(-goal.x) if optimal_angle.angle > 180 and goal.x < 0 else goal.x,
+            y=goal.y,
+            angle=optimal_angle
+        )
+
+        return goal
